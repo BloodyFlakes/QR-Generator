a00bc0bbf211dce48f1fd0f4901bbc6555049221
Rashed Al-Masri <157921405+BloodyFlakes@users.noreply.github.com>
2024-04-06 16:27:12 +0300

    main.js

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 let imgBox = document.getElementById("imgBox");
+let qrLink = document.getElementById("qrLink");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
 let btn = document.querySelector("button");
 
 function generateQR() {
   if (qrText.value.length > 0) {
+    let url = qrText.value;
+    // Check if the URL doesn't start with http:// or https://
+    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+      // Prepend http:// to the URL
+      url = "http://" + url;
+    }
+    qrLink.href = url; // Update the href attribute with the corrected URL
     qrImage.src =
       "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-      qrText.value;
+      encodeURIComponent(url); // Make sure to encode the URL
     imgBox.classList.add("show-img");
   } else {
     qrText.classList.add("error");
@@ -16,4 +24,5 @@ function generateQR() {
     }, 1000);
   }
 }
+
 btn.addEventListener("click", generateQR);
